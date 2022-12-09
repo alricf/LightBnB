a3b3d9335b4eec200ae91c33cc8388d862810741
ACF <alricfernandes11@gmail.com>
2022-12-09 13:19:16 -0500

    Implementing lightBNB web boilerplate by connecting it to database and refactoring getAllProperties function

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -1,6 +1,16 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
 
+////////////////////////////Connect to PG database///////////////////////
+const { Pool } = require('pg');
+const pool = new Pool({
+  user: 'af',
+  password: '',
+  host: 'localhost',
+  database: 'lightbnb'
+});
+///////////////////////////////////////////////////////////////////////
+
 /// Users
 
 /**
@@ -66,13 +76,18 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
-}
+// Rewrite/Refactor getAllProperties
+const getAllProperties = (options, limit = 10) => {
+  return pool
+    .query(`SELECT * FROM properties LIMIT $1`, [limit])
+    .then((result) => {
+      console.log(result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
 exports.getAllProperties = getAllProperties;
 
 
